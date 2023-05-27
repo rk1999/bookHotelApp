954bc7d891631632ab11af813fdd2015dae7faf3
rk1999 <rishabh.k1999@gmail.com>
2023-05-27 21:52:35 +0530

    implemented jwt based authentication and authorization

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,28 @@
 import express from "express"
+import { getUsers, getUser, updateUser, deleteUser } from "../controllers/user.js"
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js"
 
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.send("Get users endpoint")
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+    res.send("Hello user, you are logged in")
 })
+
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+    res.send("Hello user, you are logged in and can delete the account")
+})
+
+router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+    res.send("Hello Admin, you are logged in and can delete all accounts")
+})
+
+//UPDATE
+router.put("/:id", verifyUser, updateUser)
+//DELETE
+router.delete("/:id", verifyUser, deleteUser)
+//GET
+router.get("/:id", verifyUser, getUser)
+//GET ALL
+router.get("/", verifyAdmin, getUsers)
 
 export default router
